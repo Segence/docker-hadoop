4efec1e1409351fb310fdcf00da5a34390b00973
R Vadai <rvadai@segence.com>
2016-07-30 14:09:40 +0100

    Updated RHEL init script

--- cluster-setup/standalone-cluster/setup-rhel.sh
+++ cluster-setup/standalone-cluster/setup-rhel.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 yum update -y
-yum install bind-utils docker docker-registry -y
+yum install bind-utils telnet nmap ntp docker docker-registry -y
+systemctl enable ntpd.service
 systemctl enable docker.service
 /sbin/sysctl -w net.ipv4.ip_forward=1
+
+# Set system timezone to UTC
+/bin/cp /usr/share/zoneinfo/UTC /etc/localtime
 
 mkdir -p /hadoop/data
 mkdir -p /hadoop/deployments
 mkdir -p /hadoop/slaves-config
 
-useradd hadoop
+# Creating hadoop user and adding it to the wheel group to automatically
+# be able to run sudo commands
+useradd -G wheel hadoop
 chown -R hadoop:hadoop /hadoop
 
+systemctl start ntpd.service
 systemctl start docker.service
