47c7efa76a5bcaf70a1ca44292f75b5d77d90b0a
R Vadai <rvadai@segence.com>
2016-09-16 19:56:09 +0100

    Creating latest tag for Docker image

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+IMAGE_NAME=segence/hadoop
 VERSION=$(cat version.txt)
-docker build -t segence/hadoop:$VERSION docker-container
+
+docker build -t $IMAGE_NAME:$VERSION docker-container
+docker tag -f $IMAGE_NAME:$VERSION $IMAGE_NAME:latest
 
